f556d87cbb22ef145fdb79a4031eb73d4dd0dc53
Shoeb Hashem <shoeb.hashem@gmail.com>
2023-10-31 14:17:47 +0100

    delete.sh and script.sh updated

--- delete.sh
+++ delete.sh
@@ -1,1 +1,3 @@
 kubectl delete --cascade='foreground' -f userapi-deploy.yaml
+kubectl delete --cascade='foreground' -f movieapi-deploy.yaml
+kubectl delete --cascade='foreground' -f ingress.yaml
--- script.sh
+++ script.sh
@@ -2,6 +2,23 @@
 
 #!/bin/sh
 
+
+kubectl apply -f movieapi-deploy.yaml -l data=config
+kubectl apply -f movieapi-deploy.yaml -l app=moviestore
+
+# Extract the clusterIP
+export moviestoreip=$( kubectl get \
+                              services/moviestore-service \
+                              --template='{{.spec.clusterIP}}' )
+
+# Retrieve the ConfigMap, replace "NOTSET" with the clusterIP, and re-apply.
+kubectl get configmap/movieapi-config -o yaml\
+    | sed -r "s/NOTSET/$moviestoreip/" | kubectl apply -f -
+
+# Finally, start the movieapi-service
+kubectl apply -f movieapi-deploy.yaml -l app=movieapi
+
+
 kubectl apply -f userapi-deploy.yaml -l data=config
 kubectl apply -f userapi-deploy.yaml -l app=userstore
 
@@ -19,5 +36,7 @@ kubectl delete secret session-secret
 kubectl create secret generic session-secret --from-literal=SESSION_SECRET=your-secret-value
 
 
-# Finally, start the qfapp
+# Finally, start the userapi
 kubectl apply -f userapi-deploy.yaml -l app=userapi
+
+#kubectl apply -f nginx-deploy.yaml 
